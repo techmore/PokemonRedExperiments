7bb95570b6af50855b1dbbefd96ff8afc82f5535
techmore <sdolbec1@gmail.com>
2023-10-20 09:15:52 -0400

    pyboy update num_cpu update run_baseline_parallel.py
    with new tile maps we can significantly increase performance.

--- baselines/run_baseline_parallel.py
+++ baselines/run_baseline_parallel.py
@@ -8,6 +8,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.callbacks import CheckpointCallback
 from argparse_pokemon import *
+import os
 
 def make_env(rank, env_conf, seed=0):
     """
@@ -39,8 +40,10 @@ if __name__ == '__main__':
             }
     
     env_config = change_env(env_config, args)
-    
-    num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
+
+    num_cpu = os.cpu_count() * 6
+    print('num_cpu: ', num_cpu)
+    #num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
     env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
     
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
